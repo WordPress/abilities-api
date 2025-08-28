0cad3fea0ee218c9031e5c3ee444fae7c0f3df36
Greg Ziółkowski <grzegorz@gziolo.pl>
2025-08-28 09:12:27 +0200

    Define WP_ABILITIES_API_VERSION and update comments
    Added version definition for the plugin and improved comments.

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -10,16 +10,20 @@
  * file.
  */
 
+// Version of the plugin.
+if ( ! defined( 'WP_ABILITIES_API_VERSION' ) ) {
+	define( 'WP_ABILITIES_API_VERSION', '0.1.0' );
+}
+
 // Load core classes if they are not already defined (for non-Composer installs or direct includes).
 if ( ! class_exists( 'WP_Ability' ) ) {
 	require_once __DIR__ . '/abilities-api/class-wp-ability.php';
 }
-
 if ( ! class_exists( 'WP_Abilities_Registry' ) ) {
 	require_once __DIR__ . '/abilities-api/class-wp-abilities-registry.php';
 }
 
-// Ensure procedural functions are available.
+// Ensure procedural functions are available, too.
 if ( ! function_exists( 'wp_register_ability' ) ) {
 	require_once __DIR__ . '/abilities-api.php';
 }
